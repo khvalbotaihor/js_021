88f1cea6ec0b2f9c35eb7969243f9ca1261259e5
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-02-03 15:27:56 +0200

    - added new unit tests

--- src/01-hello-tests/01.test.ts
+++ src/01-hello-tests/01.test.ts
@@ -1,4 +1,4 @@
-import {multiply, sum} from "./01";
+import {multiply, splitIntoWords, sum} from "./01";
 
 
 test("sum should be correct",()=>{
@@ -13,7 +13,6 @@ test("sum should be correct",()=>{
     //expect
     expect(result1).toBe(3)
     expect(result2).toBe(5)
-    expect(result2).toBe(5)
 })
 test("multiply should be correct",()=>{
     const a = 1;
@@ -27,4 +26,19 @@ test("multiply should be correct",()=>{
     //expect
     expect(result1).toBe(2)
     expect(result2).toBe(6)
+})
+
+test("words split works correctly",()=>{
+    const sent1 = "Hello my friend!"
+    const sent2 = "JS - the best programming language"
+
+    // action
+    const result1 = splitIntoWords(sent1)
+    const result2 = splitIntoWords(sent2)
+
+    //expect
+    expect(result1.length).toBe(3)
+    expect(result1[0]).toBe("hello")
+    expect(result1[1]).toBe("my")
+    expect(result1[2]).toBe("friend")
 })
