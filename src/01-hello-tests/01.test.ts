7828075cb83edc1c31c505e4229e7d57cc281cf0
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-02-03 09:35:13 +0200

    - updated readme.md

--- src/01-hello-tests/01.test.ts
+++ src/01-hello-tests/01.test.ts
@@ -1,4 +1,4 @@
-import {sum} from "./01";
+import {multiply, sum} from "./01";
 
 
 test("sum should be correct",()=>{
@@ -13,4 +13,17 @@ test("sum should be correct",()=>{
     //expect
     expect(result1).toBe(3)
     expect(result2).toBe(5)
+})
+test("multiply should be correct",()=>{
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    // action
+    const result1 = multiply(a,b)
+    const result2 = multiply(c,b)
+
+    //expect
+    expect(result1).toBe(2)
+    expect(result2).toBe(6)
 })
