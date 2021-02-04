e481f3d12afbb92a0ac1c51fcc88739dc67460ef
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-02-04 17:40:30 +0200

    - third part of block

--- src/03/03.ts
+++ src/03/03.ts
@@ -1,4 +1,5 @@
 import {studentType} from "../02/02";
+import {CityType, governmentBuildingsType, HouseType} from "../02/02_02";
 
 export const sum = (a: number, b:number) =>{
     return a + b
@@ -28,3 +29,26 @@ export function livesIn(student: studentType, city:string) {
    return student.address.city.title===city
 }
 
+export function addMoneyToBudget1(building: governmentBuildingsType[], type:"HOSPITAL" | "FIRE-STATION", increaseBudged: number  ) {
+    return building.map(b =>b.type===type ? b.budget += increaseBudged : 0)
+}
+
+export const addMoneyToBudget = (building: governmentBuildingsType,  budget: number) => {
+    building.budget += budget
+}
+
+export const repairHouse = (house: HouseType) =>{
+    house.repaired= true
+}
+
+export const fireStaff = (building: governmentBuildingsType, staffToFire: number) =>{
+    building.staffCount -= staffToFire
+}
+
+export const hireStaff = (building: governmentBuildingsType, staffToHire: number) => {
+    building.staffCount +=staffToHire
+}
+
+export function createMessage(city: CityType) {
+        return `Hello ${city.title} citizens!. I want you to be happy all ${city.citizenNumber} people`
+}
--- src/03/03_02.test.ts
+++ src/03/03_02.test.ts
@@ -1,4 +1,5 @@
 import { CityType } from "../02/02_02";
+import {addMoneyToBudget, createMessage, fireStaff, hireStaff, repairHouse} from "./03";
 
 let city : CityType;
 
@@ -25,4 +26,49 @@ beforeEach(()=>{
         ],
         citizenNumber: 1000000
     }
+})
+
+
+test("Budget should be changed for Hospital", ()=>{
+
+    addMoneyToBudget(city.governmentBuildings[0],   100000);
+
+    expect(city.governmentBuildings[0].budget).toBe(300000)
+    expect(city.governmentBuildings[1].budget).toBe(500000)
+})
+
+test("Budget should be changed for FireStation", ()=>{
+
+    addMoneyToBudget(city.governmentBuildings[1],  -100000);
+
+    expect(city.governmentBuildings[0].budget).toBe(200000)
+    expect(city.governmentBuildings[1].budget).toBe(400000)
+})
+
+
+test("House should be repaired", ()=>{
+    expect(city.houses[1].repaired).toBe(false)
+
+    repairHouse(city.houses[1]);
+
+    expect(city.houses[1].repaired).toBeTruthy()
+})
+
+test("People should be fired", ()=>{
+
+    fireStaff(city.governmentBuildings[0], 20)
+    expect(city.governmentBuildings[0].staffCount).toBe(180);
+})
+test("People should be hired", ()=>{
+
+    hireStaff(city.governmentBuildings[0], 20)
+    hireStaff(city.governmentBuildings[1], 100)
+    expect(city.governmentBuildings[0].staffCount).toBe(220);
+    expect(city.governmentBuildings[1].staffCount).toBe(1100);
+})
+test("Greeting message should be correct for city", ()=>{
+
+    const message = createMessage(city)
+
+    expect(message).toBe("Hello New York citizens!. I want you to be happy all 1000000 people");
 })
